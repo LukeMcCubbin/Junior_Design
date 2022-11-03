98a2ff97a3b08a5c676c20bd7053e59f91d2f948
Gulnihal1 <guli_gulinihali1@baylor.edu>
2022-11-03 14:55:38 -0500

    update

--- Testcode/Testcode.ino
+++ Testcode/Testcode.ino
@@ -1,12 +1,12 @@
 //LCD Code
 /* PROJECT BY MANISH KUMAR YADAV*/
 #include<LiquidCrystal.h>
-int rs=7;
-int en=8;
-int d4=9;
-int d5=10;
-int d6=11;
-int d7=12;
+int rs=31;
+int en=33;
+int d4=23;
+int d5=25;
+int d6=27;
+int d7=29;
 int tm=1000;
 int j;
 LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
--- Testcode/stepper_motor1.ino
+++ Testcode/stepper_motor1.ino
@@ -1,22 +1,22 @@
-#include <Stepper.h>
+//#include <Stepper.h>
 
 // C++ code
 //
-const int stepPerRevolution=200; 
-
-Stepper Stepperx(stepPerRevolution, 5,11,2,8);
-int stepCount = 0; 
-void setup() {
-
-Serial.begin(9600);
-}
-void loop() {
-
-for(int i =0; i< 200; i++){
-  Stepperx.step(1);
-  delay(10);
-}
-
-delay(1000);
-
-}
+//const int stepPerRevolution=200; 
+//
+//Stepper Stepperx(stepPerRevolution, 5,11,2,8);
+//int stepCount = 0; 
+//void setup() {
+//
+//Serial.begin(9600);
+//}
+//void loop() {
+//
+//for(int i =0; i< 200; i++){
+//  Stepperx.step(1);
+//  delay(10);
+//}
+//
+//delay(1000);
+//
+//}
