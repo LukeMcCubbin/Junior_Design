cb68e4c85dfc0ab317f5548d9d934125f03f66a9
Luke McCubbin <lukemccubbin1@gmail.com>
2022-11-10 16:38:47 -0600

    Revert "Update Testcode.ino"
    This reverts commit ecb861113cceb34c14725ca9f5582f5a09d6fb9a.

--- Testcode/Testcode.ino
+++ Testcode/Testcode.ino
@@ -6,12 +6,9 @@
 #define bt_start A1 // Start Button
 #define bt_Stop A2 // Stop Button
 
-const int stepsPerRevolution=200; 
+const int stepPerRevolution=200; 
 
-Stepper StepperUp(stepsPerRevolution, 2,3,4,5);
-//Stepper Stepper1(stepsPerRevolution, 6, 7, 8, 9);
-//Stepper Stepper2(stepsPerRevolution, 10, 11, 12, 13);
-
+Stepper Stepperx(stepPerRevolution, 2,3,4,5);
 int stepCount = 0; 
 
 
@@ -23,34 +20,21 @@ int d6=27;
 int d7=29;
 int tm=1000;
 int j;
-int state = 1;
 
 int randNum = 0;
 
 LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
 
-int randGen(){
-int randnum;
-randnum = random(1, 9);
-return randnum;
-}
-
-void StartBatch(int randNum, LiquidCrystal lcd){
-  //randomSeed(analogRead(A0));
-
+void StartBatch(int* randNum, LiquidCrystal lcd){
+  randomSeed(analogRead(A0));
+  *randNum = random(1, 9);
+  delay(200);
   lcd.clear();
   lcd.print("Please load: ");
-  lcd.print(randNum);
+  lcd.print(*randNum);
+  delay(10000);
   //*loadTrack = 0;
 }
-
-void lower_level(Stepper stepper){
- for(int i =0; i< 200; i++){
-  stepper.step(1);
-  delay(10);
-}
-}
-
 
 //Setup
 void setup() {
@@ -65,28 +49,16 @@ void setup() {
 
 //Loop
 void loop() {
-  int randNum = randGen();
-  StartBatch(randNum,lcd);
-  switch (state){
-
-    case 1:
-      StartBatch(randNum,lcd);
-      lower_level(StepperUp);
-      delay(100);
-      break;
-
-      default: 
-        break;
-    
-    
-  }
-
-  /*
 StartBatch(&randNum, lcd);
 delay(200);
-lower_level(StepperUp);
+
+for(int i =0; i< 200; i++){
+  Stepperx.step(1);
+  delay(10);
+}
 
 delay(1000);
-*/
 
 }
+
+//}
