c213b0aa30ced0577413b3ba63002b9e12b8a985
Arum Okwo <samcascade366@gmail.com>
2022-11-16 21:44:18 -0600

    Update Junior_Design_Code.ino

--- Junior_Design_Code/Junior_Design_Code.ino
+++ Junior_Design_Code/Junior_Design_Code.ino
@@ -1,5 +1,5 @@
 // Junior Design Code
-
+//[Arum] Replaced code with Luke's test code with expanded loading process and edits to DC run
 //Comments 
 /*
  * Sections to write are 1. overall code, 2. Conveyor belt, 3. sensors, 4. XY  coordinate
@@ -7,161 +7,274 @@
  */
 
 //Define 
-#include <LiquidCrystal.h>
+#include<LiquidCrystal.h>
 #include <Stepper.h>
-#define bt_P A1 // Start Button
-#define bt_L A2 // Stop Button
-//testing github
 
-//LCD pins
+#define bt_start A1 // Start Button
+#define bt_Stop A2 // Stop Button
+#define SENSORPIN 40
+#define SENSORPIN2 41
+
+//stepper
+const int stepPerRevolution=200; 
+Stepper StepperLeft(stepPerRevolution, 2,3,4,5);
+Stepper StepperRight(stepPerRevolution, 6,7,8,9);
+Stepper StepperUp(stepPerRevolution, 10,11,12,13);
+int stepCount = 0; 
+
+//DC Motor
+int speedPin=52;
+int dir1 = 50;
+//int dir2 = 48;
+int mSpeed=255;
+
+//LCD
 int rs=31;
 int en=33;
 int d4=23;
 int d5=25;
 int d6=27;
 int d7=29;
+int tm=1000;
+int j;
 
-LiquidCrystal lcd(rs,en,d4,d5,d6,d7); // define lcd
+//Everything else
+int expression = 0;
+int randNum = 0;
+int count = 0;
+int tempval = 0;
 
-//DC
-const int IN1 = 48;
-const int IN2 = 50;
-const int ENA = 52;
+// variables will change:
+int sensor = 0;
 
-//Steppers
-Stepper upStepper(stepsPerRevolution, 2, 3, 4, 5);
-Stepper Stepper1(stepsPerRevolution, 6, 7, 8, 9);
-Stepper Stepper2(stepsPerRevolution, 10, 11, 12, 13);
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
 
-//Sensors
-const int S = 33;
-
-//Other
-long duration; // variable for the duration of sound wave travel
-int distance; 
-int x = 0;
-int read_ADC = 0;
-int duty_cycle; // PWM
-int duty_cycle_lcd;
-int set = 1;
-long randNumber;
-
-//First Setup - On start
-void setup(){
-  Serial.begin(9600);
-  pinMode (IN1, OUTPUT);
-  pinMode (IN2, OUTPUT);
-  pinMode (ENA, OUTPUT);
-  
-  pinMode(bt_P, INPUT_PULLUP);
-  pinMode(bt_L, INPUT_PULLUP);
-    
-  //pinMode(trigPin, OUTPUT); 
-  //pinMode(echoPin, INPUT);
-    
-// Guli -- lcd  
-  lcd.begin(16,2);  
+void StartBatch(int* randNum, LiquidCrystal lcd){
+  randomSeed(analogRead(A0));
+  *randNum = random(1, 9);
+  lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("   Generating    ");
+  lcd.print("Loading :");
   lcd.setCursor(0,1);
-  lcd.print("  Random Number   ");
-  
-// Guli -- Random Number Generator
-//luke note: can we make this into a function?
-  randomSeed(analogRead(0));
-  randNumber = random(1, 9); // print a random number from 1 to 8
-  delay(2000); 
+  lcd.print("Random number: ");
+  lcd.print(*randNum);
+  delay(1000);
+  //*loadTrack = 0;
+}
+
+void printScreen(int val, LiquidCrystal lcd){
   lcd.clear();
-  lcd.print(randNumber);
-  delay(2000); 
+  lcd.setCursor(0,0);
+  lcd.print("Processing :");
+  lcd.setCursor(0,1);
+  lcd.print("value: ");
+  lcd.print(val);
+}
+void printScreen2(int val, LiquidCrystal lcd){
   lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Loading :");
+  lcd.setCursor(0,1);
+  lcd.print("remaining: ");
+  lcd.print(val-1);
 }
 
 
-void loop(){ 
-  // Guli -- Control Buttons
-  if(digitalRead (bt_P) == 0){set = 1;}
-  if(digitalRead (bt_L) == 0){set = 0;}
-  
-  if(set==0){
-    lcd.setCursor(0,0);
-    lcd.print("     STOP      ");
-    lcd.setCursor(0,1);
-    digitalWrite(IN2, LOW);
+void lower_level(){
+  for(int i =0; i< 100; i++){
+   StepperUp.step(1);
+   //StepperLeft.step(-1);
+   //StepperRight.step(-1);
+   delay(10);
   }
-  
-  if(set==1){ lcd.print("    START   ");
-    digitalWrite(IN2, HIGH);           
+}
+void raise_level(){
+  for(int i =0; i< 100; i++){
+   StepperUp.step(-1);
+   //StepperLeft.step(1);
+   //StepperRight.step(1);
+   delay(10);
   }
-  delay(50);
-//  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-//  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-//  digitalWrite(trigPin, LOW);
-//  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
-  analogWrite(ENA, 255); //control speed
-  
-    
-    for(int i =0; i< 200; i++){
-      Stepperx.step(1);
-    delay(10);
-    }
+}
+void DC_run(int t){
+   analogWrite(speedPin,mSpeed);
+   digitalWrite(dir1,HIGH);
+   delay(t * 10);
+   digitalWrite(dir1,LOW);
+}
 
+
+
+//Setup
+void setup() {
+ lcd.begin(16,2); 
+ Serial.begin(9600); 
+
+ //DC motor
+ pinMode(speedPin,OUTPUT);
+ pinMode(dir1,OUTPUT);
+ delay(10);
+ 
+ pinMode(SENSORPIN, INPUT);     
+ digitalWrite(SENSORPIN, HIGH); // turn on the pullup
+ //StepperLeft.setSpeed(30);
+ //StepperUp.setSpeed(30);
+}
+
+
+//Loop
+void loop() {
+    //Starts a batch
+    StartBatch(&randNum, lcd);
     delay(1000);
 
-    //////////////Section for Luke
-    //object oriented
-    //Section 4 - XY cooridinate
-    // control of one stepper motor
-
-    /*
-     * 
-     * 
-
-     * 
-     * 
-     * 
-  for(int i =0; i< 200; i++){
-    Stepperx.step(1);
-    delay(10);
-  }
-
-delay(1000);
-
+    //sets values
+    count = 0;//using count to record the boxes loaded
+    //using tempVal to record previous reading
+    tempVal = 0;
   
-#include <Stepper.h>
 
-// Number of steps per output rotation
-const int stepsPerRevolution = 200;
-
-// Create Instance of Stepper library
-Stepper myStepper(stepsPerRevolution, 12, 11, 10, 9);
-
-
-void setup()
-{
- // set the speed at 20 rpm:
-  myStepper.setSpeed(20);
-  // initialize the serial port:
-  Serial.begin(9600);
-}
-
-void loop() 
-{
-  // step one revolution in one direction:
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(500);
-
-  // step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
-  delay(500);
-}
-     */
-
+    //loading state
+    //Read to get starting "previous" state
+    //Could be function with two parameters
+    //int randNum; int SENSORPIN2
+    tempVal = digitalRead(SENSORPIN2);
+        
+    while(count < (int)randNum){
+      sensor = digitalRead(SENSORPIN2);
+      if(tempVal == HIGH && sensor == LOW){
+        digitalWrite(dir1, HIGH);
+        //print
+      }else if(tempVal == LOW && sensor == HIGH){
+        delay(100);//Spacing between boxes
+        digitalWrite(dir1, LOW);
+        tempVal++;
+        //print
+      }
+      tempVal = sensor; //store current into previous
+    }
     
-   
+    sensor = digitalRead(SENSORPIN);
+    count = 0; //reset count
+
+    //travel -- maybe make this a function?
+    //parameter sensor
+    
+    while(sensor != LOW){
+      digitalWrite(dir1,HIGH);
+      analogWrite(speedPin,mSpeed);
+      sensor = digitalRead(SENSORPIN);
+    }
+    digitalWrite(dir1,HIGH);
+    
+
+    //processing state
+    while(count <= (int)randNum){
+        switch(expression) {
+            case 0:
+                //prints this case
+                printScreen(expression+1,lcd);
+                //runsDC motor
+                DC_run(200);
+                //falls
+                delay(50);
+                //lowers the lift 
+                lower_level();
+                expression ++;
+                count++;
+                break;
+                
+            case 1:
+                //prints this case
+                printScreen(expression+1,lcd);
+                DC_run(200);
+                delay(50);
+                raise_level();
+                //across
+                expression++;
+                count++;
+                break;
+                
+            case 2:
+                //prints this case
+                printScreen(expression+1,lcd);
+                DC_run(200);
+                delay(50);
+                lower_level();
+                //across
+                expression++;
+                count++;
+                break;
+                
+            case 3:
+                //prints this case
+                printScreen(expression+1,lcd);
+                DC_run(200);
+                delay(50);
+                raise_level();
+                //across
+                expression++;
+                count++;
+                break;
+                
+            case 4:
+                //prints this case
+                printScreen(expression+1,lcd);
+                DC_run(200);
+                delay(50);
+                lower_level();
+                //across
+                expression++;
+                count++;
+                break;
+                
+            case 5:
+                //prints this case
+                printScreen(expression+1,lcd);
+                DC_run(200);
+                delay(50);
+                raise_level();
+                //across
+                expression++;
+                count++;
+                break;
+                
+            case 6:
+                //prints this case
+                printScreen(expression+1,lcd);
+                DC_run(200);
+                delay(50);
+                lower_level();
+                //across
+                expression++;
+                count++;
+                break;
+                
+            case 7:
+                //prints this case
+                printScreen(expression+1,lcd);
+                DC_run(200);
+                delay(50);
+                raise_level();
+                expression ++;
+                count++;
+                break;
+                //This one may be unnecessary
+                /*
+                case 8:
+                     DC_run();
+                    delay(50);
+                    lower_level();
+                    //across
+                    expression++;
+                    break;
+                    */
+                    
+            default:
+            //Could be a rest state for replacing the pallet
+                delay(2000);
+                count++;
+                expression = 0;
+        }
+    }
 }
